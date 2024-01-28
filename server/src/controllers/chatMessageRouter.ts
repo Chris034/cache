a7a9afdbeaeedd17446b9c0853966430bb12f946
chris034 <christian.k.sarran@gmail.com>
2024-01-28 02:06:13 -0500

    setup validators

--- server/src/controllers/chatMessageRouter.ts
+++ server/src/controllers/chatMessageRouter.ts
@@ -1,14 +1,55 @@
+import { body, param, query, validationResult } from "express-validator";
+
+const getMessagesByRoomValidator = [
+    param('roomNumber', 'room number is required to be 4 digit integer').isNumeric().isLength({min:4, max:4}),
+  ]
+  
 async function getMessagesByRoom(req, res)
 {
-    return res.status(200).json({ success: true });} 
+    console.log(req.params)
+    const result = validationResult(req);
+    if (result.isEmpty()) {
+        return res.status(200).json({ success: true });
+    }
 
-async function getUserById(req, res)
+    res.status(400).send({ errors: result.array() });
+} 
+
+const createMessageValidator = [
+    body('createdOn', 'date is must be ISOString').isISO8601(),
+    body('username', 'username cannot be Empty').not().isEmpty(),
+    body('roomNumber', 'room number is required to be 4 digit integer').isNumeric().isLength({min:4, max:4}),
+  ]
+
+async function createMessage(req, res)
 {
-    return {}
-}
+    const result = validationResult(req);
+    if (result.isEmpty()) {
+        return res.status(200).json({ success: true });
+    }
+
+    res.status(400).send({ errors: result.array() });} 
+
+const deleteMessageValidator = [
+    param('messageId', 'messageId cannot be empty').not().isEmpty(),
+  ]
+  
+async function deleteMessage(req, res)
+{
+    const result = validationResult(req);
+    if (result.isEmpty()) {
+        return res.status(200).json({ success: true });
+    }
+
+    res.status(400).send({ errors: result.array() });
+} 
 
 
 export default {
     getMessagesByRoom,
-    getUserById,
-  }
+    getMessagesByRoomValidator,
+    createMessage,
+    createMessageValidator,
+    deleteMessage,
+    deleteMessageValidator
+}
--- server/src/routes/chatMessageRouter.ts
+++ server/src/routes/chatMessageRouter.ts
@@ -1,31 +1,98 @@
 import express from 'express';
-import room from '../controllers/chatMessageRouter';
+import chatMessage from '../controllers/chatMessageRouter';
 
 const chatMessageRouter = express.Router()
 
 /** GET Methods */
-/**
- * @openapi
- * '/api/chat-message/{roomNumber}/':
- *  get:
- *     tags:
- *     - Chat Message Controller
- *     summary: Get all messages in a room
- *     parameters:
- *      - room number: room number
- *        in: path
- *        description: The room number of the messages you want to get
- *        required: true
- *     responses:
- *      200:
- *        description: Fetched Successfully
- *      400:
- *        description: Bad Request
- *      404:
- *        description: Not Found
- *      500:
- *        description: Server Error
- */
-chatMessageRouter.get('/chat-message/:roomNumber', room.getMessagesByRoom)
+    /**
+     * @openapi
+     * '/api/chat-message/{roomNumber}':
+     *  get:
+     *     tags:
+     *     - Chat Message Controller
+     *     summary: Get all messages in a room
+     *     parameters:
+     *      - name: roomNumber
+     *        in: path
+     *        description: The room number for which to retrieve chat messages
+     *        required: true
+     *        schema:
+     *          type: string
+     *     responses:
+     *      200:
+     *        description: Fetched Successfully
+     *      400:
+     *        description: Bad Request
+     *      404:
+     *        description: Not Found
+     *      500:
+     *        description: Server Error
+     */
+    chatMessageRouter.get('/:roomNumber', chatMessage.getMessagesByRoomValidator, chatMessage.getMessagesByRoom)
+
+/** POST Methods */
+    /**
+     * @openapi
+     * '/api/chat-message':
+     *  post:
+     *     tags:
+     *     - Chat Message Controller
+     *     summary: Create a chat message and associate it to a room
+     *     requestBody:
+     *      required: true
+     *      content:
+     *        application/json:
+     *           schema:
+     *            type: object
+     *            required:
+     *              - content
+     *              - createdOn
+     *              - username
+     *              - roomNumber
+     *            properties:
+     *              username:
+     *                type: string
+     *              createdOn:
+     *                type: string
+     *              content:
+     *                type: string
+     *              roomNumber:
+     *                  type: integer
+     *     responses:
+     *      201:
+     *        description: Created
+     *      400:
+     *        description: Bad request
+     *      500:
+     *        description: Server Error
+     */
+chatMessageRouter.post('/', chatMessage.createMessageValidator, chatMessage.createMessage)
+
+/** DELETE Methods */
+    /**
+     * @openapi
+     * '/api/chat-message/{messageId}':
+     *  delete:
+     *     tags:
+     *     -  Chat Message Controller
+     *     summary: Delete chat-message by Id
+     *     parameters:
+     *      - name: messageId
+     *        in: path
+     *        description: The id of the chat message
+     *        required: true
+     *     responses:
+     *      200:
+     *        description: Removed
+     *      400:
+     *        description: Bad request
+     *      404:
+     *        description: Not Found
+     *      500:
+     *        description: Server Error
+     */
+    chatMessageRouter.delete('/:messageId', chatMessage.deleteMessageValidator, chatMessage.deleteMessage)
+
+
 
 export default chatMessageRouter;
